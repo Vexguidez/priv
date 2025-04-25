da9ef5d6f56cc1a768d5a87a48fa503f2229e9bd
Vexguidez <vexguide@gmail.com>
2025-04-25 02:43:36 +0200

    Add files via upload

---
+++ buttons.cs
@@ -0,0 +1,24 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-04-22 08:01:05.061863600 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint dwForceAttack = 0x186C840;
+        public const nint dwForceAttack2 = 0x186C8D0;
+        public const nint dwForceBackward = 0x186CB10;
+        public const nint duck = 0x186CDE0;
+        public const nint dwForceForward = 0x186CA80;
+        public const nint dwForceJump = 0x186CD50;
+        public const nint dwForceLeft = 0x186CBA0;
+        public const nint lookatweapon = 0x1A92E50;
+        public const nint reload = 0x186C7B0;
+        public const nint dwForceRight = 0x186CC30;
+        public const nint showscores = 0x1A92D30;
+        public const nint sprint = 0x186C720;
+        public const nint turnleft = 0x186C960;
+        public const nint turnright = 0x186C9F0;
+        public const nint use = 0x186CCC0;
+        public const nint zoom = 0x1A92DC0;
+    }
+}
